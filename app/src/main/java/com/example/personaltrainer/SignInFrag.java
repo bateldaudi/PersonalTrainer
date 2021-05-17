08ef824064381c4ff7a6b74cc1999659c7bdcb1f
bateldaudi <bateldaudi@gmail.com>
2021-05-17 13:45:07 +0300

    add nav bar

---
+++ app/src/main/java/com/example/personaltrainer/SignInFrag.java
@@ -0,0 +1,31 @@
+package com.example.personaltrainer;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class SignInFrag extends Fragment {
+
+    private Button signUpBtn;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_sign_in, container, false);
+        // Inflate the layout for this fragment
+        signUpBtn  = view.findViewById(R.id.signUpBtn);
+
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(view).navigate(R.id.action_signInFrag_to_SIgnUpFrag);
+            }
+        });
+        return view;
+    }
+}
