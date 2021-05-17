81a6b7cf630a73d452b2925d11599f3b5c410136
bateldaudi <bateldaudi@gmail.com>
2021-05-17 15:32:05 +0300

    Add sign up view
    Add sign up view

--- app/src/main/java/com/example/personaltrainer/SIgnUpFrag.java
+++ app/src/main/java/com/example/personaltrainer/SIgnUpFrag.java
@@ -3,18 +3,32 @@ package com.example.personaltrainer;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class SIgnUpFrag extends Fragment {
 
-
+    private TextView signInBtn;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_s_ign_up, container, false);
+        View view =  inflater.inflate(R.layout.fragment_s_ign_up, container, false);
+
+        signInBtn = view.findViewById(R.id.sign_in_btn);
+
+        signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(view).popBackStack(R.id.signInFrag,false);
+            }
+        });
+
+        return  view;
+
     }
 }
--- app/src/main/java/com/example/personaltrainer/SignInFrag.java
+++ app/src/main/java/com/example/personaltrainer/SignInFrag.java
@@ -8,17 +8,17 @@ import androidx.navigation.Navigation;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.TextView;
 
 public class SignInFrag extends Fragment {
 
-    private Button signUpBtn;
+    private TextView signUpBtn;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_sign_in, container, false);
         // Inflate the layout for this fragment
-        signUpBtn  = view.findViewById(R.id.signUpBtn);
+        signUpBtn  = view.findViewById(R.id.sign_in_btn);
 
         signUpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
